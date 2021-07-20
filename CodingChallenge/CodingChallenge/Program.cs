a014cf487710616bf84cf8930fe56518b7723e05
Sehar Malik <seharmalik@Sehars-MacBook-Air.local>
2021-07-20 09:20:24 -0700

    completed challenge

---
+++ CodingChallenge/CodingChallenge/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodingChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintResult(
+                Permute(new int[] { 1, 2, 3 })
+            );
+        }
+
+        static IList<IList<int>> Permute(int[] nums)
+        {
+            var list = new List<IList<int>>();
+            return DoPermute(nums, 0, nums.Length - 1, list);
+        }
+
+        static IList<IList<int>> DoPermute(int[] nums, int start, int end, IList<IList<int>> list)
+        {
+            if (start == end)
+            {
+                list.Add(new List<int>(nums));
+            }
+            else
+            {
+                for (var i = start; i <= end; i++)
+                {
+                    Swap(ref nums[start], ref nums[i]);
+                    DoPermute(nums, start + 1, end, list);
+                    Swap(ref nums[start], ref nums[i]);
+                }
+            }
+
+            return list;
+        }
+
+        static void Swap(ref int a, ref int b)
+        {
+            var temp = a;
+            a = b;
+            b = temp;
+        }
+
+        static void PrintResult(IList<IList<int>> lists)
+        {
+            Console.WriteLine("[");
+            foreach (var list in lists)
+            {
+                Console.WriteLine($"    [{string.Join(',', list)}]");
+            }
+            Console.WriteLine("]");
+        }
+    }
+}
